91ba522f47e5780bda083cd6405d6f2eea55dac0
JhonTrujillo <jhonhtg90@gmail.com>
2024-05-12 12:32:09 -0500

    Feat: Componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, { Fragment} from "react";
+import Header from "./componentes/Header";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    // <div className="App"></div>
+    < Fragment>
+        <Header
+          titulo="Cotizador de Prestamos"
+        />
+    </Fragment>
+);
 }
 
 export default App;
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
-function Header(){
+// function Header(props){
 
+//     // console.log(props);
+
+//     return(
+//         // <h1>Hola Mundo</h1>
+//         <Fragment>
+//         <h1>{props.titulo}</h1>
+//         </Fragment>
+//     );
+// }
+
+const Header = ({titulo}) =>{
     return(
-        <h1>Hola Mundo</h1>
+        <Fragment>
+        <h1>{titulo}</h1>
+        </Fragment>
     );
 }
+
+
 
 export default Header;
